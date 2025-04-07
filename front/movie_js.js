41a8381445c54e2b2db46f449d8b43a1adea2695
leeyongmin <trex0723@naver.com>
2025-04-07 14:26:26 +0900

    all 영화추천 완성

--- front/movie_js.js
+++ front/movie_js.js
@@ -1,10 +1,8 @@
 let MovieObject = {
     init: function (){
-        alert("init함수가 불려짐")
     },
 
     getall: function(){
-        alert("getall 함수가 불려짐")
         $.ajax({
         // 실행할 코드
             type: "GET",
@@ -15,27 +13,28 @@ let MovieObject = {
             movielist = response.result
 
             topdiv = document.createElement("div")
+            topdiv.style = "column-count:5"
             document.body.appendChild(topdiv)
 
-            //첫번째 영화 이미지
-            cmovie = document.createElement("div")
-            cmovie.className = "card"
+            movielist.forEach( movie => {
+                cmovie = document.createElement("div")
+                cmovie.className = "card"
 
-            mimg = document.createElement("img")
-            mimg.className = "card-img-top"
-            mimg.src = movielist[0].poster_path
-            cmovie.appendChild(mimg)
-            topdiv.appendChild(cmovie)
-
-            //두번째 영화 이미지
-            cmovie = document.createElement("div")
-            cmovie.className = "card"
-
-            mimg = document.createElement("img")
-            mimg.className = "card-img-top"
-            mimg.src = movielist[1].poster_path
-            cmovie.appendChild(mimg)
-            topdiv.appendChild(cmovie)
+                mimg = document.createElement("img")
+                mimg.className = "card-img-top"
+                mimg.src = movie.poster_path
+                mimg.onclick = function(){
+                    //window.location.href = movie.url // 현재창에서 열기
+                    window.open(movie.url) //새창에서 열기
+                }
+                mimg.onmouseover = function(){
+                    
+                }   
+                mimg.style.cursor ="pointer"                   
+                cmovie.appendChild(mimg)
+                topdiv.appendChild(cmovie)
+                
+            })
         }).fail(function (error){
         // 실패코드
             console.log(error)
