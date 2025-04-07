3f86862324a58db6fd83cc7923fa0d01886de23f
leeyongmin <trex0723@naver.com>
2025-04-07 15:49:25 +0900

    장르추천 완성

--- front/movie_js.js
+++ front/movie_js.js
@@ -3,6 +3,7 @@ let MovieObject = {
     },
 
     getall: function(){
+        
         $.ajax({
         // 실행할 코드
             type: "GET",
@@ -12,9 +13,10 @@ let MovieObject = {
             console.log(response)
             movielist = response.result
 
-            topdiv = document.createElement("div")
-            topdiv.style = "column-count:5"
-            document.body.appendChild(topdiv)
+            //topdiv = document.createElement("div")
+            //topdiv.style = "column-count:5"
+            //document.body.appendChild(topdiv)
+            topdiv = document.getElementById("alldiv")
 
             movielist.forEach( movie => {
                 cmovie = document.createElement("div")
@@ -35,6 +37,55 @@ let MovieObject = {
                 topdiv.appendChild(cmovie)
                 
             })
+
+            
+        }).fail(function (error){
+        // 실패코드
+            console.log(error)
+        });
+    },
+    
+    getgenres: function(){
+        
+        genre = document.getElementById("sgenre").value.toLowerCase()
+
+        $.ajax({
+        // 실행할 코드
+            type: "GET",
+            url: "http://localhost:8000/genresbest/" + genre,
+        }).done(function (response){
+        // 성공코드
+            console.log(response)
+            movielist = response.result
+
+            topdiv = document.getElementById("genrediv")
+            //  topdiv.innerHTML = "" //기존 영화를 없애는 첫번째 방법
+            //두번째방법
+            while(topdiv.firstChild){
+                topdiv.removeChild(topdiv.firstChild)
+            }
+
+            movielist.forEach( movie => {
+                cmovie = document.createElement("div")
+                cmovie.className = "card"
+
+                mimg = document.createElement("img")
+                mimg.className = "card-img-top"
+                mimg.src = movie.poster_path
+                mimg.onclick = function(){
+                    //window.location.href = movie.url // 현재창에서 열기
+                    window.open(movie.url) //새창에서 열기
+                }
+                mimg.onmouseover = function(){
+                    
+                }   
+                mimg.style.cursor ="pointer"                   
+                cmovie.appendChild(mimg)
+                topdiv.appendChild(cmovie)
+                
+            })
+
+            
         }).fail(function (error){
         // 실패코드
             console.log(error)
